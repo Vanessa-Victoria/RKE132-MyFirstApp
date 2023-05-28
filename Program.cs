b5de5833f71f44e2fca2a995c8e36cf43bc247f9
Vanessa-Victoria Uusjärv <uusjarvvanessa@gmail.com>
2023-05-29 01:47:08 +0300

    add MyFirstApp code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//This is comment to my code
+
+//rakendus küsib kasutajal sisestada tema nimi
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string intterpolation
+
+Console.WriteLine($"Hello {userName} !");
